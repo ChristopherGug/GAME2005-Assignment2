f75527a9c4a00c418ff78fce8cf51e589b56ad38
ChristopherGug <cgugelmeier@gmail.com>
2019-11-13 23:57:52 -0500

    Done Work

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -21,10 +21,22 @@ void StartScene::draw()
 	m_pInstructionsLabel->draw();*/
 
 	m_pShip->draw();
+	
+	if (m_displayUI)
+	{
+		ImGui::Render();
+		ImGuiSDL::Render(ImGui::GetDrawData());
+		SDL_SetRenderDrawColor(TheGame::Instance()->getRenderer(), 255, 255, 255, 255);
+	}
 }
 
 void StartScene::update()
 {
+	if (m_throw)
+		Move();
+
+	if (m_displayUI)
+		m_updateUI();
 }
 
 void StartScene::clean()
@@ -39,6 +51,9 @@ void StartScene::clean()
 
 void StartScene::handleEvents()
 {
+	ImGuiIO& io = ImGui::GetIO();
+	int wheel = 0;
+
 	SDL_Event event;
 	if (SDL_PollEvent(&event))
 	{
@@ -47,25 +62,88 @@ void StartScene::handleEvents()
 		case SDL_QUIT:
 			TheGame::Instance()->quit();
 			break;
+		case SDL_MOUSEMOTION:
+			m_mousePosition.x = event.motion.x;
+			m_mousePosition.y = event.motion.y;
+			break;
+		case SDL_MOUSEWHEEL:
+			wheel = event.wheel.y;
+			break;
+		case SDL_TEXTINPUT:
+			io.AddInputCharactersUTF8(event.text.text);
+			break;
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym)
 			{
 			case SDLK_ESCAPE:
 				TheGame::Instance()->quit();
 				break;
-			/*case SDLK_1:
-				TheGame::Instance()->changeSceneState(SceneState::PLAY_SCENE);
+			case SDLK_BACKQUOTE:
+				m_displayUI = (m_displayUI) ? false : true;
 				break;
-			case SDLK_2:
-				TheGame::Instance()->changeSceneState(SceneState::END_SCENE);
-				break;*/
+			case SDLK_w:
+				break;
+			case SDLK_s:
+				break;
+			case SDLK_a:
+				break;
+			case SDLK_d:
+				break;
+			}
+			{
+				int key = event.key.keysym.scancode;
+				IM_ASSERT(key >= 0 && key < IM_ARRAYSIZE(io.KeysDown));
+				io.KeysDown[key] = (event.type == SDL_KEYDOWN);
+				io.KeyShift = ((SDL_GetModState() & KMOD_SHIFT) != 0);
+				io.KeyCtrl = ((SDL_GetModState() & KMOD_CTRL) != 0);
+				io.KeyAlt = ((SDL_GetModState() & KMOD_ALT) != 0);
+				io.KeySuper = ((SDL_GetModState() & KMOD_GUI) != 0);
 			}
 			break;
+		case SDL_KEYUP:
+			switch (event.key.keysym.sym)
+			{
+			case SDL_TEXTINPUT:
+				break;
+			case SDLK_w:
+				break;
 
+			case SDLK_s:
+				break;
+
+			case SDLK_a:
+				break;
+			case SDLK_d:
+				break;
+			}
+			{
+				int key = event.key.keysym.scancode;
+				IM_ASSERT(key >= 0 && key < IM_ARRAYSIZE(io.KeysDown));
+				io.KeysDown[key] = (event.type == SDL_KEYDOWN);
+				io.KeyShift = ((SDL_GetModState() & KMOD_SHIFT) != 0);
+				io.KeyCtrl = ((SDL_GetModState() & KMOD_CTRL) != 0);
+				io.KeyAlt = ((SDL_GetModState() & KMOD_ALT) != 0);
+				io.KeySuper = ((SDL_GetModState() & KMOD_GUI) != 0);
+			}
+			break;
 		default:
 			break;
 		}
 	}
+
+	io.DeltaTime = 1.0f / 60.0f;
+	int mouseX, mouseY;
+	const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+	io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
+	io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+	io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+	io.MouseWheel = static_cast<float>(wheel);
+
+	io.DisplaySize.x = 1280;
+	io.DisplaySize.y = 720;
+
+	m_ImGuiKeyMap();
+	m_ImGuiSetStyle();
 }
 
 void StartScene::start()
@@ -82,4 +160,157 @@ void StartScene::start()
 	m_pShip = new Ship();
 	m_pShip->setPosition(glm::vec2(400.0f, 300.0f));
 	addChild(m_pShip);
+}
+
+void StartScene::m_ImGuiKeyMap()
+{
+	ImGuiIO& io = ImGui::GetIO();
+
+	// Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array.
+	io.KeyMap[ImGuiKey_Tab] = SDL_SCANCODE_TAB;
+	io.KeyMap[ImGuiKey_LeftArrow] = SDL_SCANCODE_LEFT;
+	io.KeyMap[ImGuiKey_RightArrow] = SDL_SCANCODE_RIGHT;
+	io.KeyMap[ImGuiKey_UpArrow] = SDL_SCANCODE_UP;
+	io.KeyMap[ImGuiKey_DownArrow] = SDL_SCANCODE_DOWN;
+	io.KeyMap[ImGuiKey_PageUp] = SDL_SCANCODE_PAGEUP;
+	io.KeyMap[ImGuiKey_PageDown] = SDL_SCANCODE_PAGEDOWN;
+	io.KeyMap[ImGuiKey_Home] = SDL_SCANCODE_HOME;
+	io.KeyMap[ImGuiKey_End] = SDL_SCANCODE_END;
+	io.KeyMap[ImGuiKey_Insert] = SDL_SCANCODE_INSERT;
+	io.KeyMap[ImGuiKey_Delete] = SDL_SCANCODE_DELETE;
+	io.KeyMap[ImGuiKey_Backspace] = SDL_SCANCODE_BACKSPACE;
+	io.KeyMap[ImGuiKey_Space] = SDL_SCANCODE_SPACE;
+	io.KeyMap[ImGuiKey_Enter] = SDL_SCANCODE_RETURN;
+	io.KeyMap[ImGuiKey_Escape] = SDL_SCANCODE_ESCAPE;
+
+	io.KeyMap[ImGuiKey_A] = SDL_SCANCODE_A;
+	io.KeyMap[ImGuiKey_C] = SDL_SCANCODE_C;
+	io.KeyMap[ImGuiKey_V] = SDL_SCANCODE_V;
+	io.KeyMap[ImGuiKey_X] = SDL_SCANCODE_X;
+	io.KeyMap[ImGuiKey_Y] = SDL_SCANCODE_Y;
+	io.KeyMap[ImGuiKey_Z] = SDL_SCANCODE_Z;
+}
+
+void StartScene::m_ImGuiSetStyle()
+{
+	ImGuiStyle& style = ImGui::GetStyle();
+
+	style.Alpha = 0.8f;
+	style.FrameRounding = 3.0f;
+	style.Colors[ImGuiCol_Text] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_WindowBg] = ImVec4(0.94f, 0.94f, 0.94f, 0.94f);
+	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	style.Colors[ImGuiCol_PopupBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+	style.Colors[ImGuiCol_Border] = ImVec4(0.00f, 0.00f, 0.00f, 0.39f);
+	style.Colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
+	style.Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	style.Colors[ImGuiCol_TitleBg] = ImVec4(0.96f, 0.96f, 0.96f, 1.00f);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 1.00f, 1.00f, 0.51f);
+	style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.82f, 0.82f, 0.82f, 1.00f);
+	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.98f, 0.98f, 0.98f, 0.53f);
+	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
+	style.Colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
+	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+	style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Column] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+	style.Colors[ImGuiCol_ColumnHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.78f);
+	style.Colors[ImGuiCol_ColumnActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.50f);
+	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+	style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+}
+
+void StartScene::m_updateUI()
+{
+	ImGui::NewFrame();
+	
+	std::string windowString = "Assignment 2";
+	
+	ImGui::Begin(&windowString[0], NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_MenuBar);
+	
+	ImGui::SetWindowPos(ImVec2(0, 0), true);
+
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::Separator();
+			ImGui::MenuItem("Exit", NULL, &m_exitApp);
+			ImGui::EndMenu();
+		}
+	
+		if (ImGui::BeginMenu("Help"))
+		{
+			ImGui::Separator();
+			ImGui::MenuItem("About", NULL, &m_displayAbout);
+			ImGui::EndMenu();
+		}
+
+		ImGui::EndMenuBar();
+	}
+
+	if (m_exitApp)
+		TheGame::Instance()->quit();
+
+	if (m_displayAbout)
+	{
+		ImGui::Begin("Assignment 2", &m_displayAbout, ImGuiWindowFlags_AlwaysAutoResize);
+		ImGui::Separator();
+		ImGui::Text("Christopher Gugelmeier");
+		ImGui::Text("101175944");
+		ImGui::End();
+	}
+
+	ImGui::PushItemWidth(700);
+	if (ImGui::SliderFloat("Velocity x", &m_velocity.x, -5000.0f, 5000.0f, "%.1f"))
+	{
+	}
+	ImGui::PushItemWidth(700);
+	if (ImGui::SliderFloat("Velocity y", &m_velocity.y, -5000.0f, 5000.0f, "%.1f"))
+	{
+	}
+
+	if (ImGui::Button("Reset"))
+		resetThrow();
+
+	if (ImGui::Button("Throw"))
+		Throw();
+
+
+
+	ImGui::End();
+}
+
+void StartScene::resetThrow()
+{
+	m_pShip->setPosition(glm::vec2(300, 200));
+	m_throw = false;
+}
+
+void StartScene::Throw()
+{
+	m_throw = true;
+}
+
+void StartScene::Move()
+{
+	m_position = m_pShip->getPosition() + (m_velocity * m_PPM * m_time) + (m_acceleration * 0.5f) * (m_Atime * m_Atime);
+	m_Atime += m_time;
+	m_pShip->setPosition(m_position);
 }
--- src/StartScene.h
+++ src/StartScene.h
@@ -24,6 +24,32 @@ private:
 	Label* m_pInstructionsLabel;*/
 
 	Ship* m_pShip;
+
+	glm::vec2 m_mousePosition;
+	glm::vec2 zero = glm::vec2(0, 0);
+
+	// ImGui utility functions
+	void m_ImGuiKeyMap();
+	void m_ImGuiSetStyle();
+	void m_updateUI();
+
+	bool m_exitApp = false;
+	bool m_displayAbout = false;
+	bool m_displayUI = true;
+	bool m_throw = false;
+
+	float m_gravity = 9.8f;
+	float m_angle = 45;
+	glm::vec2 m_position = zero;
+	glm::vec2 m_velocity = zero;
+	glm::vec2 m_acceleration = zero;
+	float m_PPM = 30;
+	float m_time = 0.016f; //Time step
+	float m_Atime = 0.016f;
+
+	void resetThrow();
+	void Throw();
+	void Move();
 };
 
 #endif /* defined (__START_SCENE__) */
